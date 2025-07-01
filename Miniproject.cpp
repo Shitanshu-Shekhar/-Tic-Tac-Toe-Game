4b42d8f294cf990a51f008aed304e2b1f6aff12e
Shitanshu-Shekhar <shekharshitanshu8@gmail.com>
2025-07-01 12:32:24 +0530

    Add files via upload

---
+++ Miniproject.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3];
+char currentPlayer = 'X';
+
+void initBoard() {
+    char ch = '1';
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ch++;
+        }
+    }
+}
+
+void displayBoard() {
+    cout << "\n";
+    for (int i = 0; i < 3; i++) {
+        cout << " ";
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j];
+            if (j < 2) cout << " | ";
+        }
+        cout << "\n";
+        if (i < 2) cout << "---|---|---\n";
+    }
+    cout << "\n";
+}
+
+bool makeMove(int position) {
+    int row = (position - 1) / 3;
+    int col = (position - 1) % 3;
+    if (board[row][col] != 'X' && board[row][col] != 'O') {
+        board[row][col] = currentPlayer;
+        return true;
+    }
+    return false;
+}
+
+bool checkWin() {
+    for (int i = 0; i < 3; i++) {
+        if ((board[i][0] == currentPlayer &&
+             board[i][1] == currentPlayer &&
+             board[i][2] == currentPlayer) ||
+            (board[0][i] == currentPlayer &&
+             board[1][i] == currentPlayer &&
+             board[2][i] == currentPlayer)) {
+            return true;
+        }
+    }
+    if ((board[0][0] == currentPlayer &&
+         board[1][1] == currentPlayer &&
+         board[2][2] == currentPlayer) ||
+        (board[0][2] == currentPlayer &&
+         board[1][1] == currentPlayer &&
+         board[2][0] == currentPlayer)) {
+        return true;
+    }
+    return false;
+}
+
+bool isDraw() {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] != 'X' && board[i][j] != 'O')
+                return false;
+    return true;
+}
+
+void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+int main() {
+    int pos;
+    char again;
+
+    do {
+        initBoard();
+        currentPlayer = 'X';
+        while (true) {
+            displayBoard();
+            cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+            cin >> pos;
+
+            if (pos < 1 || pos > 9 || !makeMove(pos)) {
+                cout << "Invalid move! Try again.\n";
+                continue;
+            }
+
+            if (checkWin()) {
+                displayBoard();
+                cout << "Player " << currentPlayer << " wins!\n";
+                break;
+            }
+
+            if (isDraw()) {
+                displayBoard();
+                cout << "It's a draw!\n";
+                break;
+            }
+
+            switchPlayer();
+        }
+
+        cout << "Play again? (y/n): ";
+        cin >> again;
+    } while (again == 'y' || again == 'Y');
+
+    return 0;
+}
